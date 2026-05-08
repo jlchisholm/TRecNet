5841f41e994b4f2c2d697d5c8cc0c16bc61c178b
jchishol <jenna.lori.chisholm@cern.ch>
2026-05-08 06:48:36 -0700

    Minor cleanup

--- build_TRecNet_env.sh
+++ build_TRecNet_env.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$(realpath $BASH_SOURCE)")"
 python -m venv TRecNet_env
 source TRecNet_env/bin/activate
 
-# # Add necessary packages
+# Add necessary packages
 pip install awkward~=2.8.3
 pip install h5py~=3.14.0
 pip install ipykernel~=6.30.1
@@ -19,9 +19,13 @@ pip install pandas~=2.3.0
 pip install scikit-learn~=1.6.1
 pip install scipy~=1.13.1
 pip install sigfig~=1.3.19
+pip install tf2onnx~=1.16.1
 pip install tk~=0.1.0
 pip install uproot~=5.6.2
 pip install vector~=1.6.2
+pip install tensorflow~=2.20.0
+pip install onnxruntime~=1.19.2
+pip install keras-tuner~=1.4.8
 
 # Deactivate environment
 deactivate
